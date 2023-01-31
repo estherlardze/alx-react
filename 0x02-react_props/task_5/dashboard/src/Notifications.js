d96513f882c4a01fe02fc86d85a8be49bdd340f9
estherlardze <estherlardze8@gmail.com>
2023-01-30 18:03:37 -0800

    props in react

--- 0x02-react_props/task_5/dashboard/src/Notifications.js
+++ 0x02-react_props/task_5/dashboard/src/Notifications.js
@@ -1,7 +1,9 @@
 import React from "react";
 import "./Notifications.css";
-import closeIcon from "../assets/close-icon.png";
-import { getLatestNotification } from "../utils/utils";
+
+//this icon doesnt exist, where is it? am coming
+import closeIcon from "./close-icon.png";
+import { getLatestNotification } from "./utils";
 
 function Notifications() {
   return (
--- 0x02-react_props/task_5/dashboard/src/index.js
+++ 0x02-react_props/task_5/dashboard/src/index.js
@@ -1,14 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App/App";
-import Notifications from "./Notifications/Notifications";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App/App';
 
-ReactDOM.render(
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <div id="root-notifications">
-      <Notifications />
-    </div>
     <App />
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 );
