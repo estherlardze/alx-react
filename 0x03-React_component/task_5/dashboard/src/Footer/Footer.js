08c7cfebbc63a7ea8ca300440df08bf90873fbb1
estherlardze <estherlardze8@gmail.com>
2023-02-02 18:32:45 -0800

    react component

---
+++ 0x03-React_component/task_5/dashboard/src/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Footer.js';
+
+export default function Footer(){
+  return(
+      <div className="App-footer">Copyright 2020 - holberton School</div>
+  );
+
+
+}
+
